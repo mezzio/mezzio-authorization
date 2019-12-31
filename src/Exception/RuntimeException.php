ca67003823651ef312ec38bebbba397f2bd878c2
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:43:23 -0600

    Rewriting as Laminas Project package

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -1,13 +1,14 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-authorization for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-authorization/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-authorization for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-authorization/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-authorization/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace Zend\Expressive\Authorization\Exception;
+namespace Mezzio\Authorization\Exception;
 
 class RuntimeException extends \RuntimeException implements ExceptionInterface
 {
