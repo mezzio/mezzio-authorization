f5d1cbb86ccf1c24fcce114f58ffedbf64b57677
George Steel <george@net-glue.co.uk>
2025-10-13 09:15:41 +0100

    Mark shipped classes as soft `@final` and finalize tests
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Mezzio\Authorization;
 
+/** @final */
 class ConfigProvider
 {
     /**
--- src/Exception/InvalidConfigException.php
+++ src/Exception/InvalidConfigException.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Mezzio\Authorization\Exception;
 
+/** @final */
 class InvalidConfigException extends RuntimeException
 {
 }
